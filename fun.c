0bff59eb31f5917056eb39a1d188e94f8d0ba6fb
Daniel De Burgo <sc17ddb@leeds.ac.uk>
2018-04-08 16:59:45 +0100

    Almost finished the replace inp function. After that there is just the respond function and tying up to do. On track

--- fun.c
+++ fun.c
@@ -15,26 +15,83 @@ void getinp(){ //gets user input
     count = 0; //Reset the count for next time
 }
 
-matchAndNo searchdb(FILE *db, char *toFind){
+char *splitby(char *splitting, char tosplit){
+  char *toreturn = "";
+  for (int i = 0; i < strlen(splitting); i++){
+    if (!(strcmp(splitting[i], tosplit))){
+      for (int j = i; j < strlen(splitting); j++){
+          toreturn = strcat(toreturn, splitting[j]);
+      }
+    }
+  }
+  return toreturn;
+}
+
+matchAndNo searchdb(FILE *db, char *toFind, int omit){
   char line[512];
   int lineNum = 1;
   while(fgets(line, 512, db) != NULL) {
 		if((strstr(line, toFind)) != NULL) {
-      matchAndNo r = {lineNum, line};
-      return r;
+      if (omit == 0){
+        matchAndNo r = {lineNum, line};
+        return r;
+      }
+      omit--;
 		}
 		lineNum++;
 	}
+  return NULL;
 }
 
-void replaceinp(FILE *db1, FILE *db2, FILE *db3, char *toRep){
-  matchAndNo searched = searchdb(db1, toRep);
-  char toRep[512] = searched.b;
+matchAndNo searchdb2(FILE *db, char *toFind, int omit){
+  char line[512];
+  int lineNum = 1;
+  while(fgets(line, 512, db) != NULL) {
+		if((strstr(line, toFind)) != NULL) {
+      if (omit == 0){
+        char *checking = splitby(line, "~");
+        if (!(strcmp(checking, line))){
+          matchAndNo r = {lineNum, line};
+          return r;
+        }
+        else{
+          omit++;
+        }
+      }
+      omit--;
+		}
+		lineNum++;
+	}
+  return NULL;
+}
+
+void replaceinp(FILE *db1, FILE *db2, FILE *db3, char *toRep, char *repWith){
+  matchAndNo searched = searchdb(db1, toRep, 0);
   int lineNo = searched.a;
+  fprintf(db2, "%s~%i\n", repWith, lineNo);
+  int omit = 0;
 
   //TODO FINISH THIS
+  matchAndNo searched2[512];
+  matchAndNo searched2res = searchdb2(db2, lineNo, omit);
+  while (searched2res != NULL) {
+    searched2[omit] = searched2res;
+    omit++;
+    searched2res = searchdb2(db2, lineNo, omit);
+  }
 
+  matchAndNo searched3[512];
+  for (int i = 0; i < (sizeof(searched2) / sizeof(searched2[0]); i++){
+    lineNo2 = searched2[i].a;
+    matchAndNo searched3res = searchdb2(db3, lineNo2, omit);
+    while (searched3res != NULL) {
+      searched3[omit] = searched3res;
+      omit++;
+      searched3res = searchdb2(db2, lineNo, omit);
+    }
+  }
 
+  //Return all the numbers, count the amount of them,  replace them all with 1/count
 }
 
 void notsurehowtorespond(FILE *db1, FILE *db2, FILE *db3, char *notSureAbout){
@@ -42,13 +99,13 @@ void notsurehowtorespond(FILE *db1, FILE *db2, FILE *db3, char *notSureAbout){
   do {
     getinp();
   } while(strstr(inp, "{-}") != NULL);
-  matchAndNo searched = searchdb(db1, inp);
+  matchAndNo searched = searchdb(db1, inp, 0);
   int lineNo = searched.a;
   fprintf(db1, "%s\n", notSureAbout);
-  fprintf(db2, "%s,%i\n", inp, lineNo);
-  matchAndNo searched = searchdb(db2, inp);
+  fprintf(db2, "%s~%i\n", inp, lineNo);
+  matchAndNo searched = searchdb(db2, inp, 0);
   lineNo = searched.a;
-  fprintf(db3, "1,%i\n", lineNo);
+  fprintf(db3, "1~%i\n", lineNo);
 }
 
 char *respond(char *call){
