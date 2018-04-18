03ab1028a7c675ed0b86cd59a0d4b4e00ef9ddf0
Dan De Burgo <sc17ddb@leeds.ac.uk>
2018-04-18 10:49:58 +0100

    More work done on replace inp

--- test.c
+++ test.c
@@ -13,6 +13,7 @@ typedef struct matchAndNo {
 } matchAndNo;
 
 int getlinesoffile(FILE *where){
+  rewind(where);
   char line[512];
   int counter = 0;
 
@@ -73,6 +74,7 @@ matchAndNo searchdb(FILE *db, char *toFind, int omit){
   while(fgets(line, 512, db) != NULL) {
 		if((strstr(line, toFind)) != NULL) {
       if (omit == 0){
+        //printf("%s", line);
         matchAndNo r = {lineNum, line};
         return r;
       }
@@ -126,10 +128,15 @@ void notsurehowtorespond(FILE *db1, FILE *db2, FILE *db3, char *notSureAbout){
   fprintf(db3, "1~%i\n", lineNo);
 }
 
-void replaceinp(FILE *db1, FILE *db2, FILE *db3, char *toRepCall, char *toRepResponse, char *repWith){
+void replaceinp(FILE *db1, FILE*db2. )
+
+
+/* void replaceinp(FILE *db1, FILE *db2, FILE *db3, char *toRepCall, char *toRepResponse, char *repWith){
   //Finds toRep as a call, stores in searched
   matchAndNo searched = searchdb(db1, toRepCall, 0);
+  //printf("%s %d", searched.b, searched.a);
   int lineNo = searched.a;
+  fseek(db2, 0, SEEK_END);
   fprintf(db2, "%s~%i\n", repWith, lineNo);
   int omit = 0;
 
@@ -151,60 +158,63 @@ void replaceinp(FILE *db1, FILE *db2, FILE *db3, char *toRepCall, char *toRepRes
     sprintf(tempNum, "%d", lineNo);
     searched2res = searchdb2(db2, tempNum, omit);
   }
-  printf("%s", searched2[0].b);
+  //printf("%s", searched2[0].b);
 
   //Finds all probabilities which are assigned to the relevant responses, stored in searched3 NEEDS WORK
   matchAndNo searched3[512];
   omit = 0;
   for (int i = 0; i < (sizeof(searched2) / sizeof(searched2[0])); i++){
     int lineNo2 = searched2[i].a;
-    if (lineNo2 > 0 && lineNo2 <= getlinesoffile(db3)){
-      //printf("%d\n", lineNo2);
+    //printf("%d\n", lineNo2);
+    char tempNum[512];
+    sprintf(tempNum, "%d", lineNo2);
+    //printf("%s %i\n", tempNum, omit);
+    matchAndNo searched3res = searchdb2(db3, tempNum, omit);
+    printf("%s\n", searched3res.b);
+    while (searched3res.b != NULL) {
+      //printf("%s", searched3res.b);
+      //printf("Hit the while loop \n");
+      printf("%d\n", omit);
+      searched3[omit] = searched3res;
+      omit++;
       char tempNum[512];
-      sprintf(tempNum, "%d", lineNo2);
-      printf("%s %i\n", tempNum, omit);
-      matchAndNo searched3res = searchdb2(db3, tempNum, omit);
-      printf("%s", searched3res.b);
-      while (searched3res.b != NULL) {
-        //printf("Hit the while loop \n");
-        searched3[omit] = searched3res;
-        omit++;
-        char tempNum[512];
-        sprintf(tempNum, "%d", lineNo);
-        searched3res = searchdb2(db3, tempNum, omit);
-      }
+      sprintf(tempNum, "%d", lineNo);
+      searched3res = searchdb2(db3, tempNum, omit);
     }
   }
-  printf("%s", searched3[0].b);
+  printf("%s", searched3[1].b);
   //Delete all of the probabilities
-  /*
+
   for (int i = 0; i < (sizeof(searched3) / sizeof(searched3[0])); i++){
     int lineNo3 = searched3[i].a;
-    FILE *db4 = fopen("avers2.txt", "w+");
-    int iterLineNo = 0;
-    char *currentLine;
-    int length;
+    //printf("Is %i > 0 and < %i \n", lineNo3, getlinesoffile(db3));
+    if (lineNo3 > 0 && lineNo3 <= getlinesoffile(db3)){
+      printf("%i\n", lineNo3);
+      FILE *db4 = fopen("avers2.txt", "w+");
+      int iterLineNo = 0;
+      char *currentLine;
+      int length;
 
-    while (fgets(currentLine, sizeof(currentLine), db3) != NULL) {
-      length = strlen(currentLine);
-      if (length && (currentLine[length - 1] != '\n')){
-        //Ignore
-      }
-      else{
-        iterLineNo++;
-        if (lineNo3 != iterLineNo){
-          fputs(currentLine, db4);
+      while (fgets(currentLine, sizeof(currentLine), db3) != NULL) {
+        length = strlen(currentLine);
+        if (length && (currentLine[length - 1] != '\n')){
+          //Ignore
+        }
+        else{
+          iterLineNo++;
+          if (lineNo3 != iterLineNo){
+            fputs(currentLine, db4);
+          }
         }
       }
+      fclose(db4);
+      remove("avers.txt");
+      rename("avers2.txt", "avers.txt");
     }
-    fclose(db4);
-    remove("avers.txt");
-    rename("avers2.txt", "avers.txt");
   }
-
+  /*
   //Do the maths
   matchAndNo toRepSearched = searchdb2(db2, toRepResponse, 0);
-  printf("%d\n", toRepSearched.a);
   char tempNum2[512];
   sprintf(tempNum2, "%d", toRepSearched.a);
   matchAndNo toRepSearched2 = searchdb2(db3, tempNum2, 0);
@@ -213,7 +223,6 @@ void replaceinp(FILE *db1, FILE *db2, FILE *db3, char *toRepCall, char *toRepRes
   //ERROR
   for (int i = 0; i < (sizeof(searched3) / sizeof(searched3[0])); i++){
     if (toRepSearched2.a == searched3[i].a && (!(strcmp(toRepSearched2.b,searched3[i].b)))){
-      printf("%s", searched3[i].b);
       char *toAppend = splitby(searched3[i].b ,'~');
       char temp = '0';
       char beginning[512];
@@ -225,7 +234,6 @@ void replaceinp(FILE *db1, FILE *db2, FILE *db3, char *toRepCall, char *toRepRes
       diff = atoi(beginning);
       char tempNum[512];
       sprintf(tempNum, "%f", (diff/2));
-      //searched3[i].b = strcat(itoa((atoi(beginning) / 2)), strcat("~", toAppend));
       searched3[i].b = strcat(tempNum , strcat("~", toAppend)); //concatenate int of beginning / 2 with ~ and to append
       diff = diff - atoi(searched3[i].b);
     }
@@ -244,14 +252,9 @@ void replaceinp(FILE *db1, FILE *db2, FILE *db3, char *toRepCall, char *toRepRes
       char tempNum[512];
       sprintf(tempNum, "%f", diff);
       searched3[i].b = strcat(tempNum , strcat("~", toAppend));
-      //searched3[i].b = strcat(itoa((atoi(beginning) * diff)), strcat("~", toAppend));
     }
   }
 
-  //END OF ERROR
-
-
-  printf("Finished for loop 2\n");
   //Rewrite out searched3
   for (int i = 0; i < (sizeof(searched3) / sizeof(searched3[0])); i++){
     fprintf(db3, "%s\n", searched3[i].b);
@@ -262,7 +265,7 @@ void replaceinp(FILE *db1, FILE *db2, FILE *db3, char *toRepCall, char *toRepRes
 int main(int argc, char const *argv[]) {
   FILE *testfile1 = fopen("test.txt", "r+");
   FILE *testfile2 = fopen("test2.txt", "r+");
-  FILE *testfile3 = fopen("test3.txt", "r+");
+  FILE *testfile3 = fopen("avers.txt", "r+");
 
   //for (int i = 0; i < 6; i++) {
     //printf("%s", getline(testfile, i));
